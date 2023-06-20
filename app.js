84ad1bf8d7963db3f783067ad7969957166d4d86
박재형 <wogud114@gmail.com>
2023-06-20 23:27:11 +0900

    nico_momentum

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hi");
+const a = 5;
+const b = 2; 
+//const b = "2"; type is text. 
+const myName = "Josh";
+
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
